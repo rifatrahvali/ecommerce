b1f31d43d31539a9fe1513a2da895c8e87ee3594
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-03-19 18:04:43 +0300

    orta slider tasarımı Swiper&html&css

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.home-swiper', {
+    speed: 400,
+    spaceBetween:10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
