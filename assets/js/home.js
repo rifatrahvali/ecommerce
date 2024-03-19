32b31540a6c6ed4e9e545acb31fcacc8781deeec
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-03-19 18:38:35 +0300

    topbar ve header tasarımının son işlemleri yapıldı

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,3 +6,15 @@ const swiper = new Swiper('.home-swiper', {
         prevEl: ".swiper-button-prev"
     }
 });
+
+const brandSwiper = new Swiper('.nav-brand-swiper', {
+    speed: 900,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    autoplay:{
+        delay:1000,
+        pauseOnMouseEnter:true,
+    }
+});
